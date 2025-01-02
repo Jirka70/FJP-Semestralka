3497beeffdc75952f0822cb44a0b79e69c0698c0
Jan Vašátko <vasatkoj@students.zcu.cz>
2025-01-02 09:04:09 -0600

    Main class - create parse tree

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 ```bash
 $ mvn package
-$ java -cp target/FJP-semestralka-1.0-SNAPSHOT-jar-with-dependencies.jar org.example.Main
+$ java -cp target/FJP-semestralka-1.0-SNAPSHOT-jar-with-dependencies.jar org.example.Main <input_file>
 ```
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,30 @@
 package org.example;
 
+import org.antlr.v4.runtime.CharStream;
+import org.antlr.v4.runtime.CharStreams;
+import org.antlr.v4.runtime.CommonTokenStream;
+import org.antlr.v4.runtime.tree.ParseTree;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        /* TODO: kontrola argumentů... */
+        String inputName = args[0];
+        CharStream input = null; // vytvoření character streamu
+        try {
+            input = CharStreams.fromFileName(inputName);
+        } catch (Exception e) {
+            System.out.println("Chyba pri otevirani souboru: " + e);
+        }
+
+        IavaLexer lexer = new IavaLexer(input); // vytvoření lexeru
+        CommonTokenStream tokens = new CommonTokenStream(lexer); // vytvoření token streamu
+        IavaParser parser = new IavaParser(tokens); // vytvoření parseru
+        ParseTree tree = parser.compilationUnit(); // start parsing at the 'compilationUnit' rule and create parse tree
+
+
+        /* TODO: implementovat Visitory rozšiřující IavaParserBaseVisitor<T> a překrývající vybrané visitxxx metody.
+              Pomocí nich projít tree a vytvořit reprezentaci programu v paměti. Z té pak vygenerovat instrukce */
+        System.out.println(tree.toStringTree(parser));
+
     }
 }
